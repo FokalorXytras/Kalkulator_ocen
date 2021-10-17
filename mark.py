8cfc77dbde49c516ebd0899cc13442ad5991c5b4
Fokalor Xytras <fokalor@gmail.com>
2021-10-17 11:51:56 +0200

    Fix marks

--- mark.py
+++ mark.py
@@ -1,35 +1,35 @@
 def mark(x, y):
     z = (y/x)*100
 
-    if z >= 91:
+    if z >= 90.50:
         if z >= 97.75:
             a = "5+"
         elif z <= 93.25:
             a = "5-"
         else:
             a = "5"
-    elif 91 > z >= 76:
+    elif 90.49 > z >= 75.50:
         if z >= 86.5:
             a = "4+"
         elif z <= 79.5:
             a = "4-"
         else:
             a = "4"
-    elif 76 > z >= 61:
+    elif 75.49 > z >= 60.50:
         if z >= 71.5:
             a = "3+"
         elif z <= 63.5:
             a = "3-"
         else:
             a = "3"
-    elif 61 > z >= 51:
+    elif 60.49 > z >= 50.50:
         if z >= 57.75:
             a = "2+"
         elif z <= 53.25:
             a = "2-"
         else:
             a = "2"
-    elif 51 > z >= 0:
+    elif 50.49 > z >= 0:
         if z >= 37.5:
             a = "1+"
         elif z <= 12.5:
